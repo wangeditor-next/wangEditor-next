1ec5e62250c44e7d488b83879804c721ab7d0770
yaolongfei <2991205548@qq.com>
2026-03-20 09:49:01 +0800

    fix(core): avoid stale selection restore during ime

--- packages/core/src/text-area/syncSelection.ts
+++ packages/core/src/text-area/syncSelection.ts
@@ -12,7 +12,6 @@ import { DOMElement } from '../utils/dom'
 import { IS_FIREFOX } from '../utils/ua'
 import {
   EDITOR_TO_ELEMENT,
-  EDITOR_TO_PENDING_SELECTION,
   IS_FOCUSED,
 } from '../utils/weak-maps'
 import { hasSelectableTarget, hasTarget } from './helpers'
@@ -202,18 +201,7 @@ export function DOMSelectionToEditor(textarea: TextArea, editor: IDomEditor) {
   const anchorNodeSelectable = hasSelectableTarget(editor, anchorNode)
   const focusNodeInEditor = hasTarget(editor, focusNode)
 
-  if (isComposing) {
-    if (anchorNodeSelectable && focusNodeInEditor) {
-      const range = DomEditor.toSlateRange(editor, domSelection, {
-        exactMatch: false,
-        suppressThrow: true,
-      })
-
-      EDITOR_TO_PENDING_SELECTION.set(editor, range)
-    }
-
-    return
-  }
+  if (isComposing) { return }
 
   if (anchorNodeSelectable && focusNodeInEditor) {
     const range = DomEditor.toSlateRange(editor, domSelection, {
